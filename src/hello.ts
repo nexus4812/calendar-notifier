be7403218ab14f466f12fe412eca5d75a08e5d7e
nexus4812 <powerbatteryboy@gmail.com>
2021-02-23 12:48:31 +0900

    init

---
+++ src/hello.ts
@@ -0,0 +1,8 @@
+
+const  greeter = function (person: string) {
+    return "Hello, " + person + "!";
+};
+function myFunction() {
+    const user: string = 'Panda 🐼';
+    Logger.log(greeter(user));
+}
